150e2f98d53068aa197c7b7bf1d52a9b3c6bf1e7
Adriana Cruz <drih1502@gmail.com>
2025-04-25 03:50:39 -0300

    Utilizando parseFloat para validar números

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function imc () {
 
     if (nome !== '' && altura !== '' && peso !== '') {
 
-        const valorIMC = (peso / (altura**2)).toFixed(1);
+        const valorIMC = parseFloat (peso / (altura**2)).toFixed(1);
 
     let classificacao = 
-     imc < 18.5 ? 'abaixo do peso.' :
-     imc <= 24.9 ? 'com peso ideal.' :
-     imc <= 29.9 ? 'levemente acima do peso.' :
-     imc <= 34.9 ? 'com obesidade grau I.' :
-     imc <= 39.9 ? 'com obesidade grau II.' :
+     valorIMC < 18.5 ? 'abaixo do peso.' :
+     valorIMC <= 24.9 ? 'com peso ideal.' :
+     valorIMC <= 29.9 ? 'levemente acima do peso.' :
+     valorIMC <= 34.9 ? 'com obesidade grau I.' :
+     valorIMC <= 39.9 ? 'com obesidade grau II.' :
     'com obesidade grau III.';
 
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
